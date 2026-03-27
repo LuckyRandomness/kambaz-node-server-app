e43f7d385421e5bb77017979b49e9e82158f11c4
LuckyRandomness <hammel.k@northeastern.edu>
2026-03-27 13:17:38 -0400

    after office hours

--- kambaz/enrollments/routes.js
+++ kambaz/enrollments/routes.js
@@ -3,16 +3,51 @@ export default function EnrollmentsRoutes(app, db) {
     const dao = EnrollmentsDao(db);
     const onEnrollUserInCourse = (req, res) => {
         const currentUser = req.session["currentUser"];
+        if(!currentUser) { 
+            console.log("NO USER ERROR");
+            return;
+        }
         const { courseId } = req.params;
         const newEnrollment = dao.enrollUserInCourse(currentUser._id, courseId);
         res.send(newEnrollment);
     }
-    const onUnenrollUserInCourse = (req, res) => {
-        const currentUser = req.session["currentUser"];
+    /*const onUnenrollUserInCourse = (req, res) => {
+        let { user } = req.body || {};
+        if (!user) {
+            const currentUser = req.session["currentUser"];
+            if (!currentUser) {
+                return res.status(401).json({ error: "Unauthorized" });
+        }
+        user = currentUser._id;
+        }
         const { courseId } = req.params;
         const status = dao.unenrollUserInCourse(currentUser._id, courseId);
+        if(!status) {
+            res.status(401).send("EMPTY STATUS");
+            return;
+        }
         res.send(status);
-    }
+    }*/
+    const onUnenrollUserInCourse = async (req, res) => {
+        const { courseId } = req.params;
+        let { user } = req.body || {};
+        
+        // Use current user from session if user not provided in body
+        if (!user) {
+            const currentUser = req.session["currentUser"];
+            if (!currentUser) {
+                return res.status(401).json({ error: "Unauthorized" });
+            }
+            user = currentUser._id;
+        }
+        
+        try{
+            const result = await dao.unenrollUserInCourse(user, courseId);
+            res.json(result);   
+        } catch(error) {
+            res.status(401).json({ error: "CANNOT"});
+        }
+    };
     app.post("/api/courses/:courseId/enrollment", onEnrollUserInCourse);
     app.delete("/api/courses/:courseId/enrollment", onUnenrollUserInCourse);
 }
