cad4ac7e85862a5ed82fbebc6d2025f2dd00723d
Kelsey Hammel <hammel.k@northeastern.edu>
2026-04-17 20:45:33 -0400

    starting questions

--- kambaz/quizzes/schema.js
+++ kambaz/quizzes/schema.js
@@ -10,7 +10,12 @@ const schema = new mongoose.Schema(
     points: Number,
     description: String,
     questionNum: Number,
-    published: Boolean
+    published: Boolean,
+    type: String,
+    assignmentGroup: String,
+    shuffleAnswers: Boolean,
+    timeLimit: Boolean,
+    timeLimitAmt: Number
  }
 );
 export default schema;
